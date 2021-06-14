46ed1fbf895b3853b48c47bab683c832ee310f85
Mathias Johansson <mathias.l.johansson@advania.com>
2021-06-14 16:04:43 +0200

    Testar validatering och klasser

---
+++ NewDir/mathiasj.ps1
@@ -0,0 +1,19 @@
+function mathiasfunc {
+    [CmdletBinding(
+        SupportsShouldProcess=$True
+    )]
+    param (
+        [Parameter(Mandatory=$True,Position=0)]
+        $source
+    )
+
+    begin {
+
+    }
+    process {
+
+    }
+    end {
+
+    }
+}
